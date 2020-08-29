99fb933356aa7d35c512240305fd7690672ef2a2
matheusnunesismael <nunes.matheus.ismael@gmail.com>
2020-08-29 18:18:04 -0300

    Recebimento de arquivo temporario

--- server/src/controllers/EspacialSimulacoesController.js
+++ server/src/controllers/EspacialSimulacoesController.js
@@ -3,13 +3,16 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response){
-        const { file } = req;
+        const { file } = request;
 
-    
+        /*
         const { nome_exibicao, dados_simulacao } = request.body;
         id = await connection('espacial_simulacoes')
             .select('espacial_simulacoes.*');
+        */
 
-        return response.json(id);
+        console.log(file);
+
+        return response.json("fim");
     }
 }
